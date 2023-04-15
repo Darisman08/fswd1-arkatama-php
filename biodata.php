47555dd9247fffed585da376a5984c6982623cef
Darisman08 <darisman08@gmail.com>
2023-04-15 14:21:03 +0700

    tugas php

---
+++ biodata.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Biodata</title>
+	<link rel="stylesheet" href="/assets/css/css.css">
+</head>
+
+<body>
+	<div class="container">
+		<img src="/assets/images/aris.png" alt="Foto 3x4">
+		<h1>Biodata Diri</h1>
+		<table>
+			<tr>
+				<th>Nama</th>
+				<td>Aris Darisman</td>
+			</tr>
+			<tr>
+				<th>TTL</th>
+				<td>Bandung, Januari</td>
+			</tr>
+			<tr>
+				<th>Jenis Kelamin</th>
+				<td>Laki-laki</td>
+			</tr>
+			<tr>
+				<th>Alamat</th>
+				<td>Puri Serpong 1 Blok D3/4</td>
+			</tr>
+			<tr>
+				<th>Riwayat Pendidikan</th>
+				<td>
+					<ul>
+						<li>Sekolah Dasar SDN Kencana Indah 1</li>
+						<li>Sekolah Menengah Pertama 3</li>
+						<li>Sekolah Menengah Atas Pasundan</li>
+					</ul>
+				</td>
+			</tr>
+			<tr>
+				<th>Hobby</th>
+				<td>Futsal</td>
+			</tr>
+		</table>
+	</div>
+</body>
+
+</html>
